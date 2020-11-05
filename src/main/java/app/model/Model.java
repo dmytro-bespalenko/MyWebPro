c39a4f9ccaf61f6b795284d7e63a0c5c40d7f229
Dmytro1602 <Dmytro1602@users.noreply.github.com>
2020-11-05 16:29:27 +0200

    Model

--- src/main/java/app/model/Model.java
+++ src/main/java/app/model/Model.java
@@ -1,5 +1,33 @@
 package app.model;
 
+import app.entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Model {
+    private static Model instance = new Model();
+
+    private List<User> model;
+
+    public Model() {
+        model = new ArrayList<>();
+    }
+
+    public static Model getInstance() {
+        return instance;
+    }
+
+    public void add(User user) {
+        model.add(user);
+    }
+    public List<String> list(){
+
+        return model.stream()
+                .map(User::getName)
+                .collect(Collectors.toList());
+    }
+
 
 }
